a8de192a1ac54069133f59b70cb33dd7bcea93c2
nhistory <sehwan.lee.dev@gmail.com>
2023-02-04 13:01:57 -0400

    basic setup for express, nodemon, mongoose

--- BackEnd/README.md
+++ BackEnd/README.md
@@ -1,1 +1,5 @@
 # This is BackEnd folder of lend a hand capstone project
+
+## System setup
+
+- Node: v18.13.0
---
+++ BackEnd/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running. http://localhost:${port}`);
+});
