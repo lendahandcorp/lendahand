b5b8b80823ed62b536051a01ff59be54d55b9648
Omar Almasalmeh <omarmasalmeh@hotmail.com>
2023-02-05 02:46:14 -0400

    Initial Tests

---
+++ BackEnd/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+
+//load in any env variables from our .env file
+require('dotenv').config();
+
+//create an instenece of our express app
+const app = express();
+//Temp Port (can be replaced with the .env port)
+const port = 3000;
+
+// connect to MongoDB
+mongoose.set("strictQuery", false);
+console.log(`MONGO_DB: ${process.env.MONGO_DB}`);
+mongoose.connect(process.env.MONGO_DB);
+
+//import the Products model
+const users = require('./models/user')
+
+app.get('/users', (req, res) => {
+  // res.send('Hello World!');
+  users.find((err, data) => {
+
+    res.json(data)
+  })
+});
+
+// var indexRouter = require('./routes/index'); //index = index.js
+// var apiRouter = require('./routes/api/api');  //api = api folder
+
+app.listen(port, () => {
+  console.log(`Server is running. http://localhost:${port}`);
+});
---
+++ BackEnd/models/user.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'First name is required!!'],
+    max: 100,
+  },
+//   last_name: {
+//     type: String,
+//     required: [true, 'Last name is required!!'],
+//     max: 100,
+//   },
+//   address: {
+//     type: String,
+//     required: true
+//   },
+//   phone: {
+//     type: Number,
+//     required: true
+//   },
+//   photo: {
+//     type: String,
+//     required: true
+//   },
+  email: {
+    type: String,
+    trim: true,
+    unique: true,
+    required: [true, 'Email address is required!!'],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please fill a valid email address',
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required!!'],
+    max: 255,
+  }
+//   ,
+//   been_helped: {
+//     type: Number,
+//     required: false
+//   },
+//   helped_others: {
+//     type: Number,
+//     required: false
+//   }
+});
+
+module.exports = mongoose.model('User', userSchema);
