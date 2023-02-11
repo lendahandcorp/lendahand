68e1127ebe5d419707d66820813aee92288e0b0b
brunopFreitas <w0448225@nscc.ca>
2023-02-11 09:10:43 -0400

    Editing Custom CSS File

--- FrontEnd/src/components/SignIn.js
+++ FrontEnd/src/components/SignIn.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../css/signin.css';
+// import '../css/signin.css';
 import { useNavigate } from "react-router-dom";
 import authService from '../services/authService'
 import {emailValidator, passwordValidator} from './Validator'
