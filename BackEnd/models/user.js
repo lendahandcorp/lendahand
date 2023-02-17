171d4a4eb061dcd03d65ed23083a17d1d86d6747
brunopFreitas <w0448225@nscc.ca>
2023-02-17 11:59:14 -0400

    Fixing merging conflicts

--- BackEnd/models/user.js
+++ BackEnd/models/user.js
@@ -2,16 +2,11 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-<<<<<<< HEAD
   firstName: {
-=======
-  name: {
->>>>>>> frontend
     type: String,
     required: [true, 'First name is required!!'],
     max: 100,
   },
-<<<<<<< HEAD
   lastName: {
     type: String,
     required: [true, 'Last name is required!!'],
@@ -29,25 +24,6 @@ const userSchema = new Schema({
   //     type: String,
   //     required: true
   //   },
-=======
-//   last_name: {
-//     type: String,
-//     required: [true, 'Last name is required!!'],
-//     max: 100,
-//   },
-//   address: {
-//     type: String,
-//     required: true
-//   },
-//   phone: {
-//     type: Number,
-//     required: true
-//   },
-//   photo: {
-//     type: String,
-//     required: true
-//   },
->>>>>>> frontend
   email: {
     type: String,
     trim: true,
@@ -62,7 +38,6 @@ const userSchema = new Schema({
     type: String,
     required: [true, 'Password is required!!'],
     max: 255,
-<<<<<<< HEAD
   },
   //   ,
   //   been_helped: {
@@ -76,18 +51,3 @@ const userSchema = new Schema({
 });
 
 module.exports = mongoose.model('User', userSchema);
-=======
-  }
-//   ,
-//   been_helped: {
-//     type: Number,
-//     required: false
-//   },
-//   helped_others: {
-//     type: Number,
-//     required: false
-//   }
-});
-
-module.exports = mongoose.model('User', userSchema);
->>>>>>> frontend
