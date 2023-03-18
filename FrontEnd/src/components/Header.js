292a68b4d3452a36f3ee37909f4d5e30005fec42
Joy Cho <yewoncho0404@gmail.com>
2023-03-17 23:43:16 -0300

    clean up the code for Header

--- FrontEnd/src/components/Header.js
+++ FrontEnd/src/components/Header.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import authService from '../services/authService';
 import componentService from '../services/componentService';
@@ -16,54 +16,33 @@ const Header = () => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark justify-content-between">
-    <div className="container">
-      <Link to="/#" className="d-flex align-items-center">
-       <img className="logo-img navbar-brand" src={logo} alt="logo"/>
-      </Link>
+      <div className="container">
+        <Link to="/#" className="d-flex align-items-center">
+        <img className="logo-img navbar-brand" src={logo} alt="logo"/>
+        </Link>
 
-      {/* <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-        {componentService.grabMyUserDetails().email}
-        </button>
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-          <li><a class="dropdown-item" href="#">Create Post</a></li>
-          <li><Link className="dropdown-item" onClick={ () => logout() }>Sign out</Link></li>
-        </ul>
-      </div> */}
-
-      {/* <div className="collapse navbar-collapse">  
-
-      {/* <div className="collapse navbar-collapse">   */}
-      {/* <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-            <span class="navbar-toggler-icon"></span>
-      </button> */}
-      
-      <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-
-
-      <div className="d-flex justify-content-between">       
-        <div className="navbar-nav">
-        { authService.isAuthenticated() ? 
-          <div className="nav-item active dropdown d-block">
-            <button class="btn nav-link dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-            {componentService.grabMyUserDetails().email}
-            </button>
-            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-              <li><button type="button" className="dropdown-item" onClick={() => navigate('/postcreate')}>Write a Post</button></li>
-              <li><Link className="dropdown-item" onClick={ () => logout() }>Sign out</Link></li>
-            </ul>
+        <div className="d-flex justify-content-between">       
+          <div className="navbar-nav">
+          { authService.isAuthenticated() ? 
+            <div className="nav-item active dropdown d-block">
+              <button class="btn nav-link dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+              {componentService.grabMyUserDetails().email}
+              </button>
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li><button type="button" className="dropdown-item" onClick={() => navigate('/postcreate')}>Write a Post</button></li>
+                <li><Link className="dropdown-item" onClick={ () => logout() }>Sign out</Link></li>
+              </ul>
+            </div>
+          :
+            <div className="d-flex navbar-nav">
+              <Link className="nav-link nav-item" to="/signin">Sign in</Link>
+              <Link className="nav-link nav-item" to="/register">Register</Link>
+            </div> 
+            }
           </div>
-        :
-          <div className="d-flex navbar-nav">
-            <Link className="nav-link nav-item" to="/signin">Sign in</Link>
-            <Link className="nav-link nav-item" to="/register">Register</Link>
-          </div> 
-          }
         </div>
       </div>
-      </div>
-    </div>
-  </nav>
+    </nav>
   );
 };
 
