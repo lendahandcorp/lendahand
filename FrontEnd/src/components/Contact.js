0637c3ad88a0b7dbb944955b00389d9f014f7ef4
Joy Cho <yewoncho0404@gmail.com>
2023-03-21 14:25:04 -0300

    initial commit for contact

---
+++ FrontEnd/src/components/Contact.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import '../css/header.css';
+
+const Contact = () => {
+
+  return (
+      <div className="container mt-5">
+       <div className="row">
+        <div className="col d-flex justify-content-center">
+        <p>We can help. Our Tech Team is on hand to answer your questions</p>
+        </div>
+       </div>
+       <div className="row shadow-sm p-3 mb-5 bg-white rounded w-75 d-flex justify-content-center">
+         <div className=" col-sm admin-img">
+          <img src="https://source.unsplash.com/WLUHO9A_xik/200x200" alt="lol" className="rounded profile-post-img"/>
+         </div>
+         <div className="col-sm">
+          <h6 className="admin-name">Bruno Feistas</h6>
+          <p className="fw-light">Project Manager / Back-End Engineer</p>
+          <p className="fw-light"><a href="mailto:w0448225@nscc.ca">w0448225@nscc.ca</a></p>
+         </div>
+       </div>
+       <div className="row shadow-sm p-3 mb-5 bg-white rounded w-75 d-flex justify-content-center">
+         <div className=" col-sm admin-img">
+          <img src="https://source.unsplash.com/WLUHO9A_xik/200x200" alt="lol" className="rounded profile-post-img"/>
+         </div>
+         <div className="col-sm">
+          <h6 className="admin-name">Sehwan Lee</h6>
+          <p className="fw-light">Back-End Team Lead</p>
+          <p className="fw-light"><a href="mailto:w0458329@nscc.ca">w0458329@nscc.ca</a></p>
+         </div>
+       </div>
+       <div className="row shadow-sm p-3 mb-5 bg-white rounded w-75 d-flex justify-content-center">
+         <div className="col-sm admin-img">
+          <img src="https://source.unsplash.com/WLUHO9A_xik/200x200" alt="lol" className="rounded profile-post-img"/>
+         </div>
+         <div className="col-sm">
+          <h6 className="admin-name">Tynan Sampsel</h6>
+          <p className="fw-light">Front-End Team Lead</p>
+          <p className="fw-light"><a href="mailto:w0461785@nscc.ca">w0461785@nscc.ca</a></p>
+         </div>
+       </div>
+       <div className="row shadow-sm p-3 mb-5 bg-white rounded w-75 d-flex justify-content-center">
+         <div className=" col-sm admin-img">
+          <img src="https://source.unsplash.com/WLUHO9A_xik/200x200" alt="lol" className="rounded-circle profile-post-img"/>
+         </div>
+         <div className="col-sm">
+          <h6 className="admin-name">Omar Almasalmeh</h6>
+          <p className="fw-light">Back-End Engineer</p>
+          <p className="fw-light"><a href="mailto:w0432414@nscc.ca">w0432414@nscc.ca</a></p>
+         </div>
+       </div>
+       <div className="row shadow-sm p-3 mb-5 bg-white rounded w-75 d-flex justify-content-center">
+         <div className=" col-sm admin-img">
+          <img src="https://source.unsplash.com/WLUHO9A_xik/200x200" alt="lol" className="rounded profile-post-img"/>
+         </div>
+         <div className="col-sm">
+          <h6 className="admin-name">Jason Sunnyassy</h6>
+          <p className="fw-light">Front-End Engineer</p>
+          <p className="fw-light"><a href="mailto:w0451497@nscc.ca">w0451497@nscc.ca</a></p>
+         </div>
+       </div>
+       <div className="row shadow-sm p-3 mb-5 bg-white rounded w-75 d-flex justify-content-center">
+         <div className=" col-sm admin-img">
+          <img src="https://source.unsplash.com/WLUHO9A_xik/200x200" alt="lol" className="rounded profile-post-img"/>
+         </div>
+         <div className="col-sm">
+          <h6 className="admin-name">Joy Cho</h6>
+          <p className="fw-light">Front-End Engineer</p>
+          <p className="fw-light"><a href="mailto:w0292611@nscc.ca">w0292611@nscc.ca</a></p>
+         </div>
+       </div>
+      </div>
+  );
+};
+
+export default Contact;
