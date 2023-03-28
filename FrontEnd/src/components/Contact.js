b171a8fad29abf15a1c148675863e0d64b7df17b
Joy Cho <yewoncho0404@gmail.com>
2023-03-28 07:50:07 -0300

    update styling for Contact

--- FrontEnd/src/components/Contact.js
+++ FrontEnd/src/components/Contact.js
@@ -10,7 +10,7 @@ const Contact = () => {
         <p className="text-center">We can help. Our Tech Team is on hand to answer your questions.</p>
         </div>
        </div>
-       <div className="row shadow-sm p-3 mb-5 bg-white rounded">
+       <div className="row shadow-sm p-3 mb-5 bg-white rounded w-75 m-auto">
          <div className="offset-xl-2 col-xl-3 offset-lg-2 col-lg-4 offset-md-1 col-md-4 col-sm-12 admin-img d-flex justify-content-center">
           <img src="https://avatars.githubusercontent.com/u/124096091?v=4" alt="lol" className="rounded profile-post-img" width="250"/>
          </div>
@@ -20,7 +20,7 @@ const Contact = () => {
           <p className="fw-light"><a href="mailto:w0448225@nscc.ca?subject=Land A Hand Enquiry: <Reason>">capstoneteamawesome2023@gmail.com</a></p>
          </div>
        </div>
-       <div className="row shadow-sm p-3 mb-5 bg-white rounded">
+       <div className="row shadow-sm p-3 mb-5 bg-white rounded w-75 m-auto">
         <div className="offset-xl-2 col-xl-3 offset-lg-2 col-lg-4 offset-md-1 col-md-4 col-sm-12 admin-img d-flex justify-content-center">
           <img src="https://avatars.githubusercontent.com/u/91203094?v=4" alt="lol" className="rounded profile-post-img" width="250" />
          </div>
@@ -30,7 +30,7 @@ const Contact = () => {
           <p className="fw-light"><a href="mailto:w0448225@nscc.ca?subject=Land A Hand Enquiry: <Reason>">w0448225@nscc.ca</a></p>
          </div>
        </div>
-       <div className="row shadow-sm p-3 mb-5 bg-white rounded">
+       <div className="row shadow-sm p-3 mb-5 bg-white rounded w-75 m-auto">
         <div className="offset-xl-2 col-xl-3 offset-lg-2 col-lg-4 offset-md-1 col-md-4 col-sm-12 admin-img d-flex justify-content-center">
           <img src="https://avatars.githubusercontent.com/u/39740066?v=4" alt="lol" className="rounded profile-post-img" width="250" />
          </div>
@@ -40,7 +40,7 @@ const Contact = () => {
           <p className="fw-light"><a href="mailto:w0458329@nscc.ca?subject=Land A Hand Enquiry: <Reason>">w0458329@nscc.ca</a></p>
          </div>
        </div>
-       <div className="row shadow-sm p-3 mb-5 bg-white rounded">
+       <div className="row shadow-sm p-3 mb-5 bg-white rounded w-75 m-auto">
         <div className="offset-xl-2 col-xl-3 offset-lg-2 col-lg-4 offset-md-1 col-md-4 col-sm-12 admin-img d-flex justify-content-center">
           <img src="https://source.unsplash.com/WLUHO9A_xik/200x200" alt="lol" className="rounded profile-post-img" width="250" />
          </div>
@@ -50,7 +50,7 @@ const Contact = () => {
           <p className="fw-light"><a href="mailto:w0461785@nscc.ca?subject=Land A Hand Enquiry: <Reason>">w0461785@nscc.ca</a></p>
          </div>
        </div>
-       <div className="row shadow-sm p-3 mb-5 bg-white rounded">
+       <div className="row shadow-sm p-3 mb-5 bg-white rounded w-75 m-auto">
         <div className="offset-xl-2 col-xl-3 offset-lg-2 col-lg-4 offset-md-1 col-md-4 col-sm-12 admin-img d-flex justify-content-center">
           <img src="https://avatars.githubusercontent.com/u/70869165?v=4" alt="lol" className="rounded profile-post-img" width="250" />
          </div>
@@ -60,7 +60,7 @@ const Contact = () => {
           <p className="fw-light"><a href="mailto:w0432414@nscc.ca?subject=Land A Hand Enquiry: <Reason>">w0432414@nscc.ca</a></p>
          </div>
        </div>
-       <div className="row shadow-sm p-3 mb-5 bg-white rounded">
+       <div className="row shadow-sm p-3 mb-5 bg-white rounded w-75 m-auto">
         <div className="offset-xl-2 col-xl-3 offset-lg-2 col-lg-4 offset-md-1 col-md-4 col-sm-12 admin-img d-flex justify-content-center">
           <img src="https://avatars.githubusercontent.com/u/51119436?v=4" alt="lol" className="rounded profile-post-img" width="250" />
          </div>
@@ -70,7 +70,7 @@ const Contact = () => {
           <p className="fw-light"><a href="mailto:w0451497@nscc.ca?subject=Land A Hand Enquiry: <Reason>">w0451497@nscc.ca</a></p>
          </div>
        </div>
-       <div className="row shadow-sm p-3 mb-5 bg-white rounded">
+       <div className="row shadow-sm p-3 mb-5 bg-white rounded w-75 m-auto">
         <div className="offset-xl-2 col-xl-3 offset-lg-2 col-lg-4 offset-md-1 col-md-4 col-sm-12 admin-img d-flex justify-content-center">
           <img src="https://avatars.githubusercontent.com/u/69976456?v=4" alt="lol" className="rounded profile-post-img" width="250" />
          </div>
