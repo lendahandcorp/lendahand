56d08c7be50e87a1d0ef0ece3153a482dc654860
nhistory <sehwan.lee.dev@gmail.com>
2023-03-30 11:18:55 -0300

    Added profile link inside of user dropdown list

--- FrontEnd/src/components/Header.js
+++ FrontEnd/src/components/Header.js
@@ -5,7 +5,6 @@ import componentService from '../services/componentService';
 import '../css/header.css';
 
 const Header = () => {
-
   const navigate = useNavigate();
 
   const logo = require('../img/logo.png');
@@ -16,45 +15,88 @@ const Header = () => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark justify-content-between">
+      <div className="container">
+        <Link to="/#" className="d-flex align-items-center">
+          <img className="logo-img navbar-brand" src={logo} alt="logo" />
+        </Link>
 
-    <div className="container">
-      <Link to="/#" className="d-flex align-items-center">
-       <img className="logo-img navbar-brand" src={logo} alt="logo"/>
-      </Link>
+        {/* <div className="collapse navbar-collapse">   */}
+        <div className="d-flex">
+          <button
+            type="button"
+            className="navbar-toggler ml-auto"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
 
-      {/* <div className="collapse navbar-collapse">   */}
-      <div className="d-flex">
-      <button type="button" className="navbar-toggler ml-auto" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-          <span class="navbar-toggler-icon"></span>
-      </button>
-      </div>
-
-      <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-
-
-      <div className="d-flex justify-content-between">       
-        <div className="navbar-nav">
-        { authService.isAuthenticated() ? 
-          <div className="nav-item active dropdown d-block">
-            <button className="btn nav-link dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-              {componentService.grabMyUserDetails().email}
-            </button>
-            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                <li><button type="button" className="dropdown-item" onClick={() => navigate('/postcreate')}>Write a Post</button></li>
-                <li><Link className="dropdown-item" onClick={ () => logout() }>Sign out</Link></li>
-            </ul>
-           </div>
-          :
-            <div className="d-flex navbar-nav">
-              <Link className="nav-link nav-item" to="/signin">Sign in</Link>
-              <Link className="nav-link nav-item" to="/register">Register</Link>
-            </div> 
-         }
-            
+        <div
+          class="collapse navbar-collapse justify-content-between"
+          id="navbarCollapse"
+        >
+          <div className="d-flex justify-content-between">
+            <div className="navbar-nav">
+              {authService.isAuthenticated() ? (
+                <div className="nav-item active dropdown d-block">
+                  <button
+                    className="btn nav-link dropdown-toggle"
+                    type="button"
+                    id="dropdownMenuButton1"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    {componentService.grabMyUserDetails().email}
+                  </button>
+                  <ul
+                    class="dropdown-menu"
+                    aria-labelledby="dropdownMenuButton1"
+                  >
+                    <li>
+                      <button
+                        type="button"
+                        className="dropdown-item"
+                        onClick={() => navigate('/postcreate')}
+                      >
+                        Write a Post
+                      </button>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" onClick={() => logout()}>
+                        Sign out
+                      </Link>
+                    </li>
+                    <li>
+                      <button
+                        type="button"
+                        className="dropdown-item"
+                        onClick={() => {
+                          navigate(
+                            '/profile/' +
+                              componentService.grabMyUserDetails().userId
+                          );
+                        }}
+                      >
+                        Profile
+                      </button>
+                    </li>
+                  </ul>
+                </div>
+              ) : (
+                <div className="d-flex navbar-nav">
+                  <Link className="nav-link nav-item" to="/signin">
+                    Sign in
+                  </Link>
+                  <Link className="nav-link nav-item" to="/register">
+                    Register
+                  </Link>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
-     </div>
     </nav>
   );
 };
--- FrontEnd/src/components/Post.js
+++ FrontEnd/src/components/Post.js
@@ -9,188 +9,218 @@ import dataService from '../services/dataService';
 // const blank_images = require('../img/blank_images.json')
 
 const Post = (props) => {
+  const [writer, setWriter] = useState({});
+  const [user_img, setUserImg] = useState({});
 
-    const [writer, setWriter] = useState({})
-    const [user_img, setUserImg] = useState({})
+  useEffect(() => {
+    dataService.getOneUser(props.data.writer, (user) => {
+      setWriter(user);
+      setUserImg(user.picture);
+    });
+  }, []);
 
-    useEffect(() => {
-        dataService.getOneUser(props.data.writer, user => {
-
-            setWriter(user);
-            setUserImg(user.picture)
-        })
-    }, [])
-
-    //Get the excerpt
-    let readmore;
-    const excerpt = (b) => {
-        const str = b
-        if(str.length <= 300){
-            readmore = false
-            return str
-        } else{
-            const test = str.substring(0,300);
-            readmore = true
-            return test
-        }
+  //Get the excerpt
+  let readmore;
+  const excerpt = (b) => {
+    const str = b;
+    if (str.length <= 300) {
+      readmore = false;
+      return str;
+    } else {
+      const test = str.substring(0, 300);
+      readmore = true;
+      return test;
     }
+  };
 
-
-    const getDate = () => {
-        const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
-        let date = new Date(props.data.availability);
-        let str = `${months[date.getMonth()]} 
+  const getDate = () => {
+    const months = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'June',
+      'July',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
+    let date = new Date(props.data.availability);
+    let str = `${months[date.getMonth()]} 
                     ${date.getDate()}${getDaySuffix(date.getDate())}, 
-                    ${date.getFullYear()}`
-        return str;
+                    ${date.getFullYear()}`;
+    return str;
+  };
+  const getDaySuffix = (i) => {
+    const daySuffix = ['st', 'nd', 'rd', 'th'];
+
+    if (i > 3) {
+      return daySuffix[3];
+    } else {
+      return daySuffix[i - 1];
     }
-    const getDaySuffix = (i) => {
-        const daySuffix = [ "st", "nd", "rd", "th"]
+  };
+  const getTagColorId = (num) => {
+    let newNum = num.toString(7);
+    newNum = newNum.match(/(\d)$/g)[0];
+    newNum = parseInt(newNum, 7);
+    newNum++;
+    return newNum;
+  };
+  const spotsLeft = () => {
+    return props.data.people_needed - props.data.people_accepted.length;
+  };
 
-        if(i > 3) {
-            return daySuffix[3];
-        }else {
-            return daySuffix[i - 1];
-        }
-    }
-    const getTagColorId = (num) => {
-        let newNum = num.toString(7);
-        newNum = newNum.match(/(\d)$/g)[0];
-        newNum = parseInt(newNum, 7);
-        newNum++;
-        return newNum
-    }
-    const spotsLeft = () => {
-        return props.data.people_needed - props.data.people_accepted.length
-    }
+  // const aa = () => {
+  //     //console.log(props.data.post_id)
+  // }
 
-    // const aa = () => {
-    //     //console.log(props.data.post_id)
-    // }
+  // const convertImage = (a, imageType) => {
+  //     //console.log(a);
+  //     var regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
 
-    // const convertImage = (a, imageType) => {
-    //     //console.log(a);
-    //     var regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+  //     if(a === undefined || a === null || !regex.test(a) || a === "" || typeof a != "string"){
+  //         console.log(props.data.title + " ("+imageType+") " + typeof(a));
 
-    //     if(a === undefined || a === null || !regex.test(a) || a === "" || typeof a != "string"){
-    //         console.log(props.data.title + " ("+imageType+") " + typeof(a));
+  //         if(imageType == "img"){
+  //             //console.log(props.data.title + " img");
+  //             return blank_images.image;
 
-    //         if(imageType == "img"){
-    //             //console.log(props.data.title + " img");
-    //             return blank_images.image;
+  //         } else if(imageType == "pic"){
+  //             //console.log(props.data.title + " pic");
+  //             return blank_images.picture;
+  //         }
+  //     } else {
+  //         //console.log(props.data.title + " a ("+imageType+")");
+  //         return `data:image/png;base64,${a}`;
+  //     }
+  // }
 
-    //         } else if(imageType == "pic"){
-    //             //console.log(props.data.title + " pic");
-    //             return blank_images.picture;
-    //         } 
-    //     } else {
-    //         //console.log(props.data.title + " a ("+imageType+")");
-    //         return `data:image/png;base64,${a}`;
-    //     }
-    // }
+  const username = () => {
+    return `${writer.firstName} ${writer.lastName}`;
+  };
 
-    const username = () => {
-        return `${writer.firstName} ${writer.lastName}`
-    }
+  return (
+    <article className="d-flex justify-content-center">
+      <div className="row shadow-sm p-3 mb-5 bg-white rounded w-75">
+        <div className="col-sm">
+          <Link to={`/postDetails/${props.data._id}`}>
+            <img
+              src={componentService.convertImageFromBase64(
+                props.data.media,
+                'img'
+              )}
+              alt="lol"
+              className="rounded postImage"
+              onClick={() => props.showPost(props.data._id)}
+              //onClick={() => aa()}
+            />
+          </Link>
 
-    return (
-
-        <article className="d-flex justify-content-center">
-            <div className="row shadow-sm p-3 mb-5 bg-white rounded w-75">
-                <div className="col-sm">
-
-                    <Link to={`/postDetails/${props.data._id}`}>
-                        <img src={ componentService.convertImageFromBase64(props.data.media, "img") }
-                            alt="lol" 
-                            className="rounded postImage"
-                            onClick={() => props.showPost(props.data._id)}
-                            //onClick={() => aa()}
-                             />
-                    </Link>
-
-                    <div className="btn mt-3 d-flex justify-content-around user" onClick={() => props.goToProfile(props.data.writer)}>
-                        <img src={ componentService.convertImageFromBase64(writer.picture, "pic") } alt="lol" class="rounded-circle img-fluid userImage" />
-                        <span className="mt-1 fw-bold">By {username()}</span>
-
-                    </div>
-                </div>
-                <div className="col-sm d-flex flex-column">
-                    <div className="d-flex flex-column">
-                    <Link to={`/postDetails/${props.data._id}`}><h5 class="fw-bold" onClick={() => props.showPost(props.data._id)}>{props.data.title}</h5></Link>
-                        <h6 class="fst-italic">Availability: {getDate()} </h6>
-                    </div>
-                    
-                    <p class="fw-light">
-                        <span>{excerpt(props.data.body)}</span>
-                        <span className=""><Link to={`/postDetails/${props.data._id}`}  className="text-secondary text-decoration-none">
-
-                        {
-                            readmore == true ? ' [ ... Read More ]' : ''
-                        }
-
-                        </Link></span>
-                    </p>
-                    <div className="d-flex">
-                    {
-                        props.data.tags.map((tag, i) => {
-                            return  <div key={i} className={`btn badge badge${getTagColorId(i)}`}
-                                onClick={() => props.tagClicked(tag.title)}
-                            >
-                                {" #" + tag.title}
-                            </div>
-                        })
-                    }
-                </div>
-            </div>
+          <div
+            className="btn mt-3 d-flex justify-content-around user"
+            onClick={() => props.goToProfile(props.data.writer)}
+          >
+            <img
+              src={componentService.convertImageFromBase64(
+                writer.picture,
+                'pic'
+              )}
+              alt="lol"
+              class="rounded-circle img-fluid userImage"
+            />
+            <span className="mt-1 fw-bold">By {username()}</span>
+          </div>
         </div>
+        <div className="col-sm d-flex flex-column">
+          <div className="d-flex flex-column">
+            <Link to={`/postDetails/${props.data._id}`}>
+              <h5
+                class="fw-bold"
+                onClick={() => props.showPost(props.data._id)}
+              >
+                {props.data.title}
+              </h5>
+            </Link>
+            <h6 class="fst-italic">Availability: {getDate()} </h6>
+          </div>
+
+          <p class="fw-light">
+            <span>{excerpt(props.data.body)}</span>
+            <span className="">
+              <Link
+                to={`/postDetails/${props.data._id}`}
+                className="text-secondary text-decoration-none"
+              >
+                {readmore == true ? ' [ ... Read More ]' : ''}
+              </Link>
+            </span>
+          </p>
+          <div className="d-flex">
+            {props.data.tags.map((tag, i) => {
+              return (
+                <div
+                  key={i}
+                  className={`btn badge badge${getTagColorId(i)}`}
+                  onClick={() => props.tagClicked(tag.title)}
+                >
+                  {' #' + tag.title}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </article>
-
-    )
-}
+  );
+};
 export default Post;
-        // <article>
-        //     <div class="row shadow-sm p-3 mb-5 bg-white rounded w-75 m-auto">
-        //         <div class="col-sm">
+// <article>
+//     <div class="row shadow-sm p-3 mb-5 bg-white rounded w-75 m-auto">
+//         <div class="col-sm">
 
-        //             <div>
-        //                 <img src={ componentService.convertImageFromBase64(props.data.media, "img") }
-        //                     alt="lol" 
-        //                     className="rounded home_post-img"
-        //                     onClick={() => props.showPost(props.data.post_id)}
-        //                      />
-        //             </div>
+//             <div>
+//                 <img src={ componentService.convertImageFromBase64(props.data.media, "img") }
+//                     alt="lol"
+//                     className="rounded home_post-img"
+//                     onClick={() => props.showPost(props.data.post_id)}
+//                      />
+//             </div>
 
-        //             <div class="btn mt-3 mb-auto d-flex post_user align-item-end" onClick={() => props.goToProfile(props.data.writer)}>
-        //                 <img src="https://source.unsplash.com/WLUHO9A_xik/35x35" alt="lol" class="rounded-circle home_post_user-img" />
-        //                 <div className="mb-auto home_post_user-name">By {username()}</div>
-        //             </div>
+//             <div class="btn mt-3 mb-auto d-flex post_user align-item-end" onClick={() => props.goToProfile(props.data.writer)}>
+//                 <img src="https://source.unsplash.com/WLUHO9A_xik/35x35" alt="lol" class="rounded-circle home_post_user-img" />
+//                 <div className="mb-auto home_post_user-name">By {username()}</div>
+//             </div>
 
-        //         </div>
+//         </div>
 
-        //         <div class="col-sm">
+//         <div class="col-sm">
 
-        //             <h5 class="fw-bold" onClick={() => props.showPost(props.data._id)}>{props.data.title}</h5>
-                    
-        //             <h6 class="fst-italic">Availability: {getDate()} </h6>
+//             <h5 class="fw-bold" onClick={() => props.showPost(props.data._id)}>{props.data.title}</h5>
 
-        //             <p class="fw-light">
-        //                 <span>{excerpt(props.data.body)}</span>
-        //                 <span className=""><Link to={`/postDetails/${props.data._id}`}>
-        //                 {
-        //                     readmore == true ? '..Read More' : ''
-        //                 }
-        //                 </Link></span>
-        //             </p>
-        //             {
-        //                 props.data.tags.map((tag, i) => {
-        //                     return  <div key={i} class={`btn badge badge${getTagColorId(i)}`}
-        //                         onClick={() => props.tagClicked(tag.title)}
-        //                     >
-        //                         {" #" + tag.title}
-        //                     </div>
-        //                 })
-        //             }
-                    
-        //         </div>
-        //     </div>
-        // </article>
+//             <h6 class="fst-italic">Availability: {getDate()} </h6>
+
+//             <p class="fw-light">
+//                 <span>{excerpt(props.data.body)}</span>
+//                 <span className=""><Link to={`/postDetails/${props.data._id}`}>
+//                 {
+//                     readmore == true ? '..Read More' : ''
+//                 }
+//                 </Link></span>
+//             </p>
+//             {
+//                 props.data.tags.map((tag, i) => {
+//                     return  <div key={i} class={`btn badge badge${getTagColorId(i)}`}
+//                         onClick={() => props.tagClicked(tag.title)}
+//                     >
+//                         {" #" + tag.title}
+//                     </div>
+//                 })
+//             }
+
+//         </div>
+//     </div>
+// </article>
