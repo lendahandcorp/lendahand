a187e2ca3e6af4b211cb6aa3f593f694bd684de8
Joy Cho <yewoncho0404@gmail.com>
2023-03-30 09:24:29 -0300

    Working on the paged linked in Footer

--- FrontEnd/src/components/Contact.js
+++ FrontEnd/src/components/Contact.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../css/footer.css';
 
 const Contact = () => {
 
   return (
-      <div className="container mt-5 contact">
+      <div className="container mt-5 mb-5 contact">
        <div className="row mb-5">
         <div className="col">
         <h1 className="text-center fw-bold"> Contact us</h1>
@@ -11,70 +12,70 @@ const Contact = () => {
         </div>
        </div>
        <div className="row shadow-sm p-3 mb-5 bg-white rounded w-75 m-auto">
-         <div className="offset-xl-2 col-xl-3 offset-lg-2 col-lg-4 offset-md-1 col-md-4 col-sm-12 admin-img d-flex justify-content-center">
-          <img src="https://avatars.githubusercontent.com/u/124096091?v=4" alt="lol" className="rounded profile-post-img" width="250"/>
+         <div className="offset-xl-2 col-xl-3 offset-lg-2 col-lg-4 col-sm-12 admin-img d-flex justify-content-center">
+          <img src="https://avatars.githubusercontent.com/u/124096091?v=4" alt="lol" className="rounded contact-img" />
          </div>
-         <div className="col-xl-5 col-lg-5 col-md-5 col-sm-12 text-center mx-auto mt-3">
+         <div className="col-xl-5 col-lg-5 col-sm-12 text-center mx-auto mt-3">
           <h6 className="admin-name">Main Contact</h6>
           <p className="fw-light">Team Awesome from NSCC</p>
           <p className="fw-light"><a href="mailto:w0448225@nscc.ca?subject=Land A Hand Enquiry: <Reason>">capstoneteamawesome2023@gmail.com</a></p>
          </div>
        </div>
        <div className="row shadow-sm p-3 mb-5 bg-white rounded w-75 m-auto">
-        <div className="offset-xl-2 col-xl-3 offset-lg-2 col-lg-4 offset-md-1 col-md-4 col-sm-12 admin-img d-flex justify-content-center">
-          <img src="https://avatars.githubusercontent.com/u/91203094?v=4" alt="lol" className="rounded profile-post-img" width="250" />
+        <div className="offset-xl-2 col-xl-3 offset-lg-2 col-lg-4 col-sm-12 admin-img d-flex justify-content-center">
+          <img src="https://avatars.githubusercontent.com/u/91203094?v=4" alt="lol" className="rounded contact-img" />
          </div>
-         <div className="col-xl-5 col-lg-5 col-md-5 col-sm-12 text-center mx-auto mt-3">
+         <div className="col-xl-5 col-lg-5 col-sm-12 text-center mx-auto mt-3">
           <h6 className="admin-name">Bruno Feistas</h6>
           <p className="fw-light">Project Manager / Back-End Engineer</p>
           <p className="fw-light"><a href="mailto:w0448225@nscc.ca?subject=Land A Hand Enquiry: <Reason>">w0448225@nscc.ca</a></p>
          </div>
        </div>
        <div className="row shadow-sm p-3 mb-5 bg-white rounded w-75 m-auto">
-        <div className="offset-xl-2 col-xl-3 offset-lg-2 col-lg-4 offset-md-1 col-md-4 col-sm-12 admin-img d-flex justify-content-center">
-          <img src="https://avatars.githubusercontent.com/u/39740066?v=4" alt="lol" className="rounded profile-post-img" width="250" />
+        <div className="offset-xl-2 col-xl-3 offset-lg-2 col-lg-4 col-sm-12 admin-img d-flex justify-content-center">
+          <img src="https://avatars.githubusercontent.com/u/39740066?v=4" alt="lol" className="rounded contact-img" />
          </div>
-         <div className="col-xl-5 col-lg-5 col-md-5 col-sm-12 text-center mx-auto mt-3">
+         <div className="col-xl-5 col-lg-5 col-sm-12 text-center mx-auto mt-3">
           <h6 className="admin-name">Sehwan Lee</h6>
           <p className="fw-light">Back-End Team Lead</p>
           <p className="fw-light"><a href="mailto:w0458329@nscc.ca?subject=Land A Hand Enquiry: <Reason>">w0458329@nscc.ca</a></p>
          </div>
        </div>
        <div className="row shadow-sm p-3 mb-5 bg-white rounded w-75 m-auto">
-        <div className="offset-xl-2 col-xl-3 offset-lg-2 col-lg-4 offset-md-1 col-md-4 col-sm-12 admin-img d-flex justify-content-center">
-          <img src="https://source.unsplash.com/WLUHO9A_xik/200x200" alt="lol" className="rounded profile-post-img" width="250" />
+        <div className="offset-xl-2 col-xl-3 offset-lg-2 col-lg-4 col-sm-12 admin-img d-flex justify-content-center">
+          <img src="https://images.unsplash.com/photo-1508718048-37b94aae7dfa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1134&q=80" alt="lol" className="rounded contact-img" />
          </div>
-         <div className="col-xl-5 col-lg-5 col-md-5 col-sm-12 text-center mx-auto mt-3">
+         <div className="col-xl-5 col-lg-5 col-sm-12 text-center mx-auto mt-3">
           <h6 className="admin-name">Tynan Sampsel</h6>
           <p className="fw-light">Front-End Team Lead</p>
           <p className="fw-light"><a href="mailto:w0461785@nscc.ca?subject=Land A Hand Enquiry: <Reason>">w0461785@nscc.ca</a></p>
          </div>
        </div>
        <div className="row shadow-sm p-3 mb-5 bg-white rounded w-75 m-auto">
-        <div className="offset-xl-2 col-xl-3 offset-lg-2 col-lg-4 offset-md-1 col-md-4 col-sm-12 admin-img d-flex justify-content-center">
-          <img src="https://avatars.githubusercontent.com/u/70869165?v=4" alt="lol" className="rounded profile-post-img" width="250" />
+        <div className="offset-xl-2 col-xl-3 offset-lg-2 col-lg-4 col-sm-12 admin-img d-flex justify-content-center">
+          <img src="https://avatars.githubusercontent.com/u/70869165?v=4" alt="lol" className="rounded contact-img" />
          </div>
-         <div className="col-xl-5 col-lg-5 col-md-5 col-sm-12 text-center mx-auto mt-3">
+         <div className="col-xl-5 col-lg-5 col-sm-12 text-center mx-auto mt-3">
          <h6 className="admin-name">Omar Almasalmeh</h6>
          <p className="fw-light">Back-End Engineer</p>
           <p className="fw-light"><a href="mailto:w0432414@nscc.ca?subject=Land A Hand Enquiry: <Reason>">w0432414@nscc.ca</a></p>
          </div>
        </div>
        <div className="row shadow-sm p-3 mb-5 bg-white rounded w-75 m-auto">
-        <div className="offset-xl-2 col-xl-3 offset-lg-2 col-lg-4 offset-md-1 col-md-4 col-sm-12 admin-img d-flex justify-content-center">
-          <img src="https://avatars.githubusercontent.com/u/51119436?v=4" alt="lol" className="rounded profile-post-img" width="250" />
+        <div className="offset-xl-2 col-xl-3 offset-lg-2 col-lg-4 col-sm-12 admin-img d-flex justify-content-center">
+          <img src="https://avatars.githubusercontent.com/u/51119436?v=4" alt="lol" className="rounded contact-img" />
          </div>
-         <div className="col-xl-5 col-lg-5 col-md-5 col-sm-12 text-center mx-auto mt-3">
+         <div className="col-xl-5 col-lg-5 col-sm-12 text-center mx-auto mt-3">
           <h6 className="admin-name">Jason Sunnyassy</h6>
           <p className="fw-light">Front-End Engineer</p>
           <p className="fw-light"><a href="mailto:w0451497@nscc.ca?subject=Land A Hand Enquiry: <Reason>">w0451497@nscc.ca</a></p>
          </div>
        </div>
        <div className="row shadow-sm p-3 mb-5 bg-white rounded w-75 m-auto">
-        <div className="offset-xl-2 col-xl-3 offset-lg-2 col-lg-4 offset-md-1 col-md-4 col-sm-12 admin-img d-flex justify-content-center">
-          <img src="https://avatars.githubusercontent.com/u/69976456?v=4" alt="lol" className="rounded profile-post-img" width="250" />
+        <div className="offset-xl-2 col-xl-3 offset-lg-2 col-lg-4 col-sm-12 admin-img d-flex justify-content-center">
+          <img src="https://avatars.githubusercontent.com/u/69976456?v=4" alt="lol" className="rounded contact-img" />
          </div>
-         <div className="col-xl-5 col-lg-5 col-md-5 col-sm-12 text-center mx-auto mt-3">
+         <div className="col-xl-5 col-lg-5 col-sm-12 text-center mx-auto mt-3">
           <h6 className="admin-name">Joy Cho</h6>
           <p className="fw-light">Front-End Engineer</p>
           <p className="fw-light"><a href="mailto:w0292611@nscc.ca?subject=Land A Hand Enquiry: <Reason>">w0292611@nscc.ca</a></p>
---
+++ FrontEnd/src/components/Security.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../css/footer.css';
+
+
+const Security = (props) => {
+
+    return (
+        <div class="container mt-5 security">
+            <div className="row mb-5">
+                <div className="col-9 m-auto">
+                    <h1 className="text-center fw-bold">Security</h1>
+                </div>
+            </div>
+            <div className="row mb-5">
+                <div className="col-9 m-auto text-center">
+                    <p className="fw-normal">
+                      We don't share any personal information with Third Party Systems. Detailed content will be updated in the next version.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Security;
