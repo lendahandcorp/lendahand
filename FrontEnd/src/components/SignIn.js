cf9a713381df02287c89a9e319da27e502d2b62b
nhistory <sehwan.lee.dev@gmail.com>
2023-03-30 18:15:15 -0300

    Added sign in error message feature

--- FrontEnd/src/components/SignIn.js
+++ FrontEnd/src/components/SignIn.js
@@ -1,31 +1,31 @@
 import React, { useState } from 'react';
 import '../css/signin.css';
-import { Link, useNavigate } from "react-router-dom";
-import authService from '../services/authService'
-import { emailValidator, passwordValidator } from './Validator'
+import { Link, useNavigate } from 'react-router-dom';
+import authService from '../services/authService';
+import { emailValidator, passwordValidator } from './Validator';
 
 const SignIn = (props) => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [emailError, setEmailError] = useState('')
-  const [passwordError, setPasswordError] = useState('')
-  const [errors, setErrors] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [errors, setErrors] = useState('');
   const navigate = useNavigate();
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
     setEmailError(emailValidator(event.target.value));
     setErrors('');
-  }
+  };
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
     setPasswordError(passwordValidator(event.target.value));
     setErrors('');
-  }
+  };
 
-  const handleSubmit = event => {
-    event.preventDefault()
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
     if (emailValidator(email) !== '') {
       setEmailError(emailValidator(email));
@@ -41,13 +41,15 @@ const SignIn = (props) => {
       if (!error) {
         navigate('/');
         props.updateNav();
-      } else {
-        if (error.status === 401) {
-          setErrors(error.data.message);
-        }
       }
+      // else {
+      //   if (error.status === 401) {
+      //     setErrors(error.data.message);
+      //   }
+      // }
+      setErrors('Email address and password combination is incorrect.');
     });
-  }
+  };
 
   return (
     <div className="card1 p-5">
@@ -56,9 +58,13 @@ const SignIn = (props) => {
       </div>
       <div className="card-signin card">
         <form className="form-signin" onSubmit={handleSubmit}>
-          <p className={errors ? 'text-danger text-center' : 'hidden'}>{errors}</p>
+          <p className={errors ? 'text-danger text-center' : 'hidden'}>
+            {errors}
+          </p>
           <div className="mb-3">
-            <label htmlFor="inputEmail" className="sr-only form-label">Email</label>
+            <label htmlFor="inputEmail" className="sr-only form-label">
+              Email
+            </label>
             <input
               onChange={handleEmailChange}
               value={email}
@@ -67,11 +73,16 @@ const SignIn = (props) => {
               id="inputEmail"
               className="form-control"
               placeholder="Email address"
-              autoFocus />
-            <p className={emailError ? 'text-danger validationErr' : 'hidden'}>{emailError}</p>
+              autoFocus
+            />
+            <p className={emailError ? 'text-danger validationErr' : 'hidden'}>
+              {emailError}
+            </p>
           </div>
           <div className="mb-3">
-            <label htmlFor="inputPassword" className="sr-only form-label">Password</label>
+            <label htmlFor="inputPassword" className="sr-only form-label">
+              Password
+            </label>
             <input
               onChange={handlePasswordChange}
               value={password}
@@ -79,19 +90,31 @@ const SignIn = (props) => {
               type="password"
               id="inputPassword"
               className="form-control"
-              placeholder="Password" />
-            <p className={passwordError ? 'text-danger validationErr' : 'hidden'}>{passwordError}</p>
+              placeholder="Password"
+            />
+            <p
+              className={passwordError ? 'text-danger validationErr' : 'hidden'}
+            >
+              {passwordError}
+            </p>
           </div>
           <div className="d-flex mt-5">
-            <button className="btn signin-btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+            <button
+              className="btn signin-btn btn-lg btn-primary btn-block"
+              type="submit"
+            >
+              Sign in
+            </button>
           </div>
           <div className="d-flex justify-content-center mt-4">
-            <p>Don't have an account? <Link to="/register">Sign Up</Link></p>
+            <p>
+              Don't have an account? <Link to="/register">Sign Up</Link>
+            </p>
           </div>
         </form>
       </div>
     </div>
   );
-}
+};
 
 export default SignIn;
