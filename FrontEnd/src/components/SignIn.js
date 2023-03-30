83420f7f378830ca6cdf081afce05aab7a199be3
omarmasalmeh <70869165+omarmasalmeh@users.noreply.github.com>
2023-03-30 15:45:49 -0300

    Updating signin form

--- FrontEnd/src/components/SignIn.js
+++ FrontEnd/src/components/SignIn.js
@@ -1,42 +1,53 @@
 import React, { useState } from 'react';
 import '../css/signin.css';
-import { Link, useNavigate } from 'react-router-dom';
-import authService from '../services/authService';
-import { emailValidator, passwordValidator } from './Validator';
+import { Link, useNavigate } from "react-router-dom";
+import authService from '../services/authService'
+import { emailValidator, passwordValidator } from './Validator'
 
 const SignIn = (props) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [passwordError, setPasswordError] = useState('');
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [emailError, setEmailError] = useState('')
+  const [passwordError, setPasswordError] = useState('')
+  const [errors, setErrors] = useState('')
   const navigate = useNavigate();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const isEmailValid = emailValidator(email);
-    if (isEmailValid !== '') {
-      setEmailError(isEmailValid);
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+    setEmailError(emailValidator(event.target.value));
+    setErrors('');
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+    setPasswordError(passwordValidator(event.target.value));
+    setErrors('');
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    if (emailValidator(email) !== '') {
+      setEmailError(emailValidator(email));
+      return;
     }
-    const isPasswordValid = passwordValidator(password);
-    if (isPasswordValid !== '') {
-      setPasswordError(isPasswordValid);
+
+    if (passwordValidator(password) !== '') {
+      setPasswordError(passwordValidator(password));
+      return;
     }
-    if (isEmailValid === '' && isPasswordValid === '') {
-      authService.signin({ email, password }, (error) => {
-        if (!error) {
-          navigate('/');
-          props.updateNav();
+
+    authService.signin({ email, password }, (error) => {
+      if (!error) {
+        navigate('/');
+        props.updateNav();
+      } else {
+        if (error.status === 401) {
+          setErrors(error.data.message);
         }
-        // else {
-        //     if(error.status===401) {
-        //         setErrors(error.data.message)
-        //     }
-        // }
-        setErrors('Email address and password combination is incorrect.');
-      });
-    }
-  };
+      }
+    });
+  }
 
   return (
     <div className="card1 p-5">
@@ -45,82 +56,42 @@ const SignIn = (props) => {
       </div>
       <div className="card-signin card">
         <form className="form-signin" onSubmit={handleSubmit}>
-          {/* <div className="mx-auto alertDiv">
-                    <p className={errors ? 'alert alert-danger text-center' : 'hidden'}>{errors}</p>
-                    <p className={emailError ? 'alert alert-danger text-center' : 'hidden'}>{emailError}</p>
-                    <p className={passwordError ? 'alert alert-danger text-center' : 'hidden'}>{passwordError}</p>
-                    </div> */}
-
-          {/* <h1 className="h3 mb-5 font-weight-normal text-center">Sign in</h1> */}
-
-          <p className={errors ? 'text-danger text-center' : 'hidden'}>
-            {errors}
-          </p>
-          <span>
-            <p className={emailError ? 'text-danger validationErr' : 'hidden'}>
-              {emailError}
-            </p>
-          </span>
+          <p className={errors ? 'text-danger text-center' : 'hidden'}>{errors}</p>
           <div className="mb-3">
-            <label htmlFor="inputEmail" className="sr-only form-label">
-              Email
-            </label>
+            <label htmlFor="inputEmail" className="sr-only form-label">Email</label>
             <input
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={handleEmailChange}
+              value={email}
               name="email"
               type="email"
               id="inputEmail"
               className="form-control"
               placeholder="Email address"
-              autoFocus
-              onBlur={() => {
-                const error = emailValidator(email);
-                setEmailError(error);
-              }}
-            />
+              autoFocus />
+            <p className={emailError ? 'text-danger validationErr' : 'hidden'}>{emailError}</p>
           </div>
-
-          <br />
-
-          <p className={passwordError ? 'text-danger validationErr' : 'hidden'}>
-            {passwordError}
-          </p>
           <div className="mb-3">
-            <label htmlFor="inputPassword" className="sr-only form-label">
-              Password
-            </label>
+            <label htmlFor="inputPassword" className="sr-only form-label">Password</label>
             <input
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={handlePasswordChange}
+              value={password}
               name="password"
               type="password"
               id="inputPassword"
               className="form-control"
-              placeholder="Password"
-              onBlur={() => {
-                const error = passwordValidator(password);
-                setPasswordError(error);
-              }}
-            />
+              placeholder="Password" />
+            <p className={passwordError ? 'text-danger validationErr' : 'hidden'}>{passwordError}</p>
           </div>
-
           <div className="d-flex mt-5">
-            <button
-              className="btn signin-btn btn-lg btn-primary btn-block"
-              type="submit"
-            >
-              Sign in
-            </button>
+            <button className="btn signin-btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
           </div>
-
           <div className="d-flex justify-content-center mt-4">
-            <p>
-              Don't have an account? <Link to="/register">Sign Up</Link>
-            </p>
+            <p>Don't have an account? <Link to="/register">Sign Up</Link></p>
           </div>
         </form>
       </div>
     </div>
   );
-};
+}
 
 export default SignIn;
